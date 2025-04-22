4c8e39e87e6c68181715b9a686392bfd7c865ad2
noeldaidon <noelkaarki21@gmail.com>
2025-04-22 12:49:47 +0545

    Initial commit for Noel's Portfolio

--- javascript/script.js
+++ javascript/script.js
@@ -2,15 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const image = document.getElementById("introImage");
     const video = document.getElementById("introVideo");
 
-    // On click image → hide image, show and play video
     image.addEventListener("click", () => {
         image.style.display = "none";
         video.style.display = "block";
         video.play();
     });
 
-    // After video ends → move to choosing.html
     video.addEventListener("ended", () => {
-        window.location.href = "../html/choosing.html";
+        window.location.href = "./Html/choosing.html";
     });
 });
